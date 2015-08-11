171dfebe109c7c1f06463f6dee0dca0629c6447e
glopez <glopez@devspark.com>
2015-08-10 22:31:38 -0300

    cleaning

--- app/src/main/java/com/lopez/espada/falconi/people_list_devspark/MainActivity.java
+++ app/src/main/java/com/lopez/espada/falconi/people_list_devspark/MainActivity.java
@@ -1,26 +1,18 @@
 package com.lopez.espada.falconi.people_list_devspark;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {//ActionBarActivity
+public class MainActivity extends AppCompatActivity {
 
     public static final String PERSON = "PERSON";
     public static final String PERSON_POSITION = "POSITION";
@@ -44,9 +36,6 @@ public class MainActivity extends AppCompatActivity {//ActionBarActivity
         bindViews();
 
         personList = new ArrayList<Person>();
-       /* Person p1 = new Person();
-        p1.setName("Jose Hardcodeado");
-        personList.add(p1);*/
         for(Person p : dao.getAllPersons()) {
             personList.add(p);
         }
@@ -82,30 +71,7 @@ public class MainActivity extends AppCompatActivity {//ActionBarActivity
         });
 
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        Log.d("MainActivity", "select on list");
-        if (id == R.id.add_person) {
-            Intent intent = new Intent(this, NewEditPerson.class);
-            startActivityForResult(intent, NEW_EDIT_PERSON);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         Log.d("MainActivity", "onActivityResult " + requestCode + " " + resultCode);
