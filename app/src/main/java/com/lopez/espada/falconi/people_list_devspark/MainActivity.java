ec4b1a7afcb728d7203c3ec6f5f90a13eddcf646
Lisandro Falconi <lisandrofalconi@gmail.com>
2015-08-11 17:32:20 -0300

    View switcher between Grid and List

--- app/src/main/java/com/lopez/espada/falconi/people_list_devspark/MainActivity.java
+++ app/src/main/java/com/lopez/espada/falconi/people_list_devspark/MainActivity.java
@@ -2,12 +2,18 @@ package com.lopez.espada.falconi.people_list_devspark;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.ViewSwitcher;
 
 import java.util.ArrayList;
 
@@ -22,8 +28,9 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Person> personList;
     private PersonListAdapter personListAdapter;
     private ListView personListView;
-
+    private GridView personGridView;
     private SQLPersonDAO dao; // I don't think this should be on the main thread D=
+    private ViewSwitcher viewSwitcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         }
         personListAdapter = new PersonListAdapter(this, personList);
         personListView.setAdapter(personListAdapter);
+        personGridView.setAdapter(personListAdapter);
     }
 
     @Override
@@ -50,17 +58,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindViews() {
+        viewSwitcher = (ViewSwitcher) findViewById(R.id.viewswitcher);
+        Animation slide_in_left = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left);
+        Animation slide_out_right = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right);
+
+        viewSwitcher.setInAnimation(slide_in_left);
+        viewSwitcher.setOutAnimation(slide_out_right);
+
+        
         personListView = (ListView) findViewById(R.id.person_list_view);
-        personListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("MainActivity", "click on list");
-                Intent intent = new Intent(MainActivity.this, NewEditPerson.class);
-                intent.putExtra(PERSON, personList.get(position));
-                intent.putExtra(PERSON_POSITION, position);
-                startActivityForResult(intent, NEW_EDIT_PERSON);
-            }
-        });
+        personGridView = (GridView) findViewById(R.id.person_grid_view);
+        personGridView.setNumColumns(2);
+
+        personListView.setOnItemClickListener(getPersonClickListener());
+        personGridView.setOnItemClickListener(getPersonClickListener());
+
         Button addPersonButton = (Button) findViewById(R.id.add_person);
         addPersonButton.setOnClickListener(new AdapterView.OnClickListener() {
             @Override
@@ -70,6 +84,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        RadioButton listViewRadioButton = (RadioButton) findViewById(R.id.linear_layout_rb);
+        listViewRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewSwitcher.setDisplayedChild(0);
+            }
+        });
+
+        RadioButton gridViewRadioButton = (RadioButton) findViewById(R.id.grid_layout_rb);
+        gridViewRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewSwitcher.setDisplayedChild(1);
+            }
+        });
+    }
+
+    @NonNull
+    private AdapterView.OnItemClickListener getPersonClickListener() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("MainActivity", "click on list");
+                Intent intent = new Intent(MainActivity.this, NewEditPerson.class);
+                intent.putExtra(PERSON, personList.get(position));
+                intent.putExtra(PERSON_POSITION, position);
+                startActivityForResult(intent, NEW_EDIT_PERSON);
+            }
+        };
     }
 
     @Override
